e24d7a6e2cd358dd3a946f33a8568b8aa53b4eaa
zldevil2011 <929034478@qq.com>
2016-06-13 09:45:00 +0800

    rebuild the structure

---
+++ app/src/main/java/com/newly_dawn/app/wsn_android/user/Login.java
@@ -0,0 +1,32 @@
+package com.newly_dawn.app.wsn_android.user;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.newly_dawn.app.wsn_android.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle("注册");
+        toolbar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Login.this.finish();
+            }
+        });
+    }
+
+}
